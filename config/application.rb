ccb82d03076d4c8a9b5b7ec1f900003abc964c80
fiskeben <ricco@fiskeben.dk>
2015-09-10 22:40:32 +0200

    Send mail with `sendmail`

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Pilogger
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+
+    ActionMailer::Base.delivery_method = :sendmail
   end
 end
