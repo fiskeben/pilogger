cc1197af5bf2492d2cb796e5e71fdcb4e72d01cd
fiskeben <ricco@fiskeben.dk>
2015-09-10 22:44:47 +0200

    More correct mail configuration.

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,5 @@ module Pilogger
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
 
-    ActionMailer::Base.delivery_method = :sendmail
   end
 end
